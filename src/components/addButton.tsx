7790653f6dd52666b9eda48cd4bf32d31083ac7d
Giovanna Di Mauro <giovydimauro@live.it>
2025-05-24 14:03:38 +0200

    Implementation of addButton

--- src/components/addButton.tsx
+++ src/components/addButton.tsx
@@ -1,20 +1,34 @@
 import { useState } from "react";
+import { PlusIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
-export default function AddButton() {
-  const [count, setCount] = useState(0);
+export default function CircleButton() {
+  const [showSearch, setShowSearch] = useState(false);
 
   return (
-    <>
-      <div
-        style={{
-          backgroundColor: "white",
-          borderRadius: "50%",
-          width: "50px",
-          height: "50px",
-        }}
+    <div>
+      <button
+        onClick={() => setShowSearch(!showSearch)}
+        className="w-12 h-12 flex items-center justify-center rounded-full bg-blue-600 hover:bg-blue-700 transition"
       >
-        addButton
-      </div>
-    </>
+        <PlusIcon className="w-6 h-6 text-white" />
+      </button>
+      {showSearch && (
+        <div
+          onClick={() => setShowSearch(false)}
+          className="fixed inset-0 backdrop-blur-sm flex items-center justify-center z-40"
+        >
+          <div className="bg-white rounded-xl shadow-xl w-full max-w-md relative">
+          <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 w-6 h-6" />
+          
+            <input
+              type="search"
+              placeholder="Cerca..."
+              autoFocus
+              className="w-full pl-12 p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
